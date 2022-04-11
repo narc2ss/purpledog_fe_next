083267da8ce79640ed4554912ca35d5f33be8ece
narc2ss <privat.narc2sse@gmail.com>
2022-04-11 15:14:13 +0900

    feat : 전체적인 영역을 태그로 분류한다

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,13 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Hello PURPLEDOG</h1>
+      <section className="banner_main">메인배너</section>
+      <section className="recommend">김새콤달콤님께 맞는 와인</section>
+      <section className="time_sale">타임세일</section>
+      <section className="banner_tour">인증샷 추어 배너</section>
+      <section className="category">국가별 와인</section>
+      <section className="exhibition">기획전</section>
+      <section className="hot">인기와인</section>
     </div>
   );
 };
