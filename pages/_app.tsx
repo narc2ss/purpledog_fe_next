bf46d75d2cd661fd6f2573134757b867cab9038f
narc2ss <privat.narc2sse@gmail.com>
2022-04-11 15:46:10 +0900

    env : css 초기화를 적용한다

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
+import { globalStyle } from "../src/styles/globalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={globalStyle} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
